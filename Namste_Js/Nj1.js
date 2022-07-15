cb016627de4f82ad721843271b96fbaedd0b9353
Mohd Ziya <iamziya53@gmail.com>
2022-07-15 17:37:04 -0400

    First Prog of Namste js

--- Namste_Js/Nj1.js
+++ Namste_Js/Nj1.js
@@ -1,1 +1,19 @@
-console.log("Mohd Ziya")
+
+// getName();
+// function getName(){
+//     console.log("Namaste Javascript");
+// }
+
+
+// function getName(){
+//     console.log("Namaste Javascript");
+// }
+// console.log(getName());
+
+getName()
+// console.log(x)
+var x=7
+function getName(){
+    console.log("Namaste Javascript");
+}
+console.log(x)
