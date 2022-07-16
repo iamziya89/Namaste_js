fde49bb39e1d8e64aad6c4c3a1eeed4fc773f9b9
Mohd Ziya <iamziya53@gmail.com>
2022-07-16 10:36:12 -0400

    Youtube harshit classes

---
+++ Namste_Js/Harshit_classes/Harshit_01.js
@@ -0,0 +1,10 @@
+// var firstName="Harshit";
+// console.log(firstName);
+// var firstname="Mohit"
+// console.log(firstname);
+
+// var value1=4;
+// console.log(value1**0.5);
+
+let value="Mohd Ziya";
+console.log(value);
