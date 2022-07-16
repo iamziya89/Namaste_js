5bceece3e0e876fcdd504dfdccdb67d07325a5a1
Mohd Ziya <iamziya53@gmail.com>
2022-07-16 10:44:34 -0400

    Youtube harshit classes

--- Namste_Js/Harshit_classes/Harshit_01.js
+++ Namste_Js/Harshit_classes/Harshit_01.js
@@ -6,5 +6,21 @@
 // var value1=4;
 // console.log(value1**0.5);
 
-let value="Mohd Ziya";
-console.log(value);
+// let value="Mohd Ziya";
+// console.log(value);
+
+// var firstName="Harshit";
+// firstName="Mohit";
+// console.log(firstName);
+// var firstName="Harshit";
+// var firstName="Mohit";
+// console.log(firstName);
+
+
+// let firstName="Harshit";
+// let firstName="Mohit";
+// console.log(firstName);// in let we can't declare first name twi times.
+
+let firstName="Harshit";
+ firstName="Mohit";
+console.log(firstName);
