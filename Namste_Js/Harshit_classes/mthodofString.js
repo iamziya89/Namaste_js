b73410db49c0c15055731b0936cf27885feba4b8
Mohd Ziya <iamziya53@gmail.com>
2022-07-16 11:21:46 -0400

    String method

---
+++ Namste_Js/Harshit_classes/mthodofString.js
@@ -0,0 +1,8 @@
+//  trim method
+
+let names="    MohsZiya    ";
+console.log(names.length);
+console.log(names);
+let newString=names.trim();
+console.log(newString);
+console.log(newString.length);
