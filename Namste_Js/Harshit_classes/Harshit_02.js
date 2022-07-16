700d1f96450a4b36949944b2ed6d8ee275868401
Mohd Ziya <iamziya53@gmail.com>
2022-07-16 11:11:58 -0400

    length of string practice

--- Namste_Js/Harshit_classes/Harshit_02.js
+++ Namste_Js/Harshit_classes/Harshit_02.js
@@ -1,2 +1,10 @@
 let firstName="harshit";
-console.log(firstName[4]);
+// console.log(firstName[6]); o/p=t
+// console.log(firstName[0]); o/p=i
+// console.log(firstName.length); o/p=7
+// console.log(firstName[firstName.length-1]); o/p=t
+
+
+
+
+
