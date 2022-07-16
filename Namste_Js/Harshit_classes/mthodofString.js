5f356597c5e93fd2f1e02923261c3e4710a96603
Mohd Ziya <iamziya53@gmail.com>
2022-07-16 11:34:30 -0400

    String method

--- Namste_Js/Harshit_classes/mthodofString.js
+++ Namste_Js/Harshit_classes/mthodofString.js
@@ -1,8 +1,22 @@
 //  trim method
 
-let names="    MohsZiya    ";
-console.log(names.length);
+// let names="    MohsZiya    ";
+// console.log(names.length);
+// console.log(names);
+// let newString=names.trim();
+// console.log(newString);
+// console.log(newString.length);
+
+
+// let names="MohdZiya";
+// console.log(names)
+// console.log(names.length);
+// // console.log(names.toLocaleUpperCase());
+// names=names.toLocaleUpperCase();
+// console.log(names);
+
+
+let names="MOHDZiya";
+names=names.toLowerCase();
 console.log(names);
-let newString=names.trim();
-console.log(newString);
-console.log(newString.length);
+
