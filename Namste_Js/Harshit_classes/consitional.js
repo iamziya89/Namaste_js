56f8ebe1943c3fc509ec46df2d43f9f7ab408c66
Mohd Ziya <iamziya53@gmail.com>
2022-07-20 18:44:36 -0400

    And operator

--- Namste_Js/Harshit_classes/consitional.js
+++ Namste_Js/Harshit_classes/consitional.js
@@ -1,10 +1,22 @@
 // if- else    Statements
 //  truthy and falsy value
-let age=6;
-let drink;
-if(age>8){
-    drink="Milk"
-}else{
-    drink="coffee"
+// let age=6;
+// let drink;
+// if(age>8){
+//     drink="Milk"
+// }else{
+//     drink="coffee"
+// }
+// console.log(drink);
+
+// let age=7;
+// let drink=age>=8? "Milk" : "coffee";
+// console.log(drink);
+
+
+//
+let firstName="Mohd Ziya";
+let age=23;
+if(firstName[0]==="M" && age>18){
+    console.log("Name starts with M and age above 18");
 }
-console.log(drink);
