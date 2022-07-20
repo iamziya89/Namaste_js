c4e3d60dc1d5d84e329300b9819669cf179252ef
Mohd Ziya <iamziya53@gmail.com>
2022-07-20 19:39:06 -0400

    practice on while loops

--- Namste_Js/Harshit_classes/whileloop.js
+++ Namste_Js/Harshit_classes/whileloop.js
@@ -1,7 +1,21 @@
-let i=0;
-while( i<=3){
-    console.log(i);
-    i++;
-}
-console.log(`current value of i is ${4}`);
-console.log("Hellow world");
+// let i=0;
+// while( i<=3){
+//     console.log(i);
+//     i++;
+// }
+// console.log(`current value of i is ${4}`);
+// console.log("Hellow world");
+
+
+// Sum of first n numbers
+// let n=parseInt(process.argv[2])
+// let i=0,sum=0;
+// while(i<=n){
+//     sum=sum+i;
+//     i++;
+// }
+// console.log(`The sum of first ${n} number is ${sum}`);
+
+// let n=10;
+// let sum=(n*(n+1))/2;
+// console.log(sum);
