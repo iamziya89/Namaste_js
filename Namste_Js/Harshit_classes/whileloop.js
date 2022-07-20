a7544dbd8c5b6617c54ddb7809d493e9aead81b6
Mohd Ziya <iamziya53@gmail.com>
2022-07-20 19:56:52 -0400

    practice on while loops

--- Namste_Js/Harshit_classes/whileloop.js
+++ Namste_Js/Harshit_classes/whileloop.js
@@ -24,7 +24,40 @@
 
 
 // For loops
-for(var i=0; i<=9;i++){
+// let i=0;
+// for( ; i<=9;i++){
+//     console.log(i);
+// }
+// console.log("value of i is",i)
+
+
+
+
+
+// let sum=0;
+// for(let i=0;i<=100;i++){
+//     sum=sum+i
+// }
+// console.log(sum);
+
+
+
+
+
+//   break keywords
+// for(let i=1;i<=10;i++){
+//     if(i===4){
+//         break;
+//     }
+//     console.log(i);
+// }
+
+
+
+// continue keyword
+for(let i=1;i<=10;i++){
+    if(i===4){
+        continue;
+    }
     console.log(i);
 }
-console.log("value of i is",i)
