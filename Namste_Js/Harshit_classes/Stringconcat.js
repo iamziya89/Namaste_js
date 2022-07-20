407195ac8275bc90876f031aa960558b62ff1821
Mohd Ziya <iamziya53@gmail.com>
2022-07-20 12:34:30 -0400

    string cocatenation

---
+++ Namste_Js/Harshit_classes/Stringconcat.js
@@ -0,0 +1,4 @@
+let str1="Mohd";
+let str2="Ziya";
+let fullName=str1+" "+str2;
+console.log(fullName);
--- Namste_Js/Harshit_classes/typeofoperator.js
+++ Namste_Js/Harshit_classes/typeofoperator.js
@@ -5,9 +5,19 @@
 
 
 // Cnversion from number to strings
-let age=54;
+// let age=54;
 //  console.log(typeof age);  // number
 // console.log(typeof (age+" "));  // string
 
-let str=age+"";
-console.log(typeof(str));
+// let str=age+"";
+// console.log(typeof(str));
+
+
+// Converting String into number
+// let myStr=+"34"
+// console.log(typeof myStr);
+
+// Another metho to conver the number into string
+let myStr=18;
+myStr=String(myStr);
+console.log(typeof myStr);
