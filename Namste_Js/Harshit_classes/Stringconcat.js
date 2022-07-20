681a22f330939250ab7b5874e2cc5178bc1f372b
Mohd Ziya <iamziya53@gmail.com>
2022-07-20 12:50:33 -0400

    String

--- Namste_Js/Harshit_classes/Stringconcat.js
+++ Namste_Js/Harshit_classes/Stringconcat.js
@@ -1,4 +1,17 @@
-let str1="Mohd";
-let str2="Ziya";
-let fullName=str1+" "+str2;
-console.log(fullName);
+// let str1="17";
+// let str2="10";
+// // let fullName=str1+" "+str2;
+// let fullName=+str1+" "+ +str2;
+// console.log(fullName);
+
+// let str="Mohd"
+// // str=Number(str);
+// console.log(typeof +str);
+
+// Tempaltes strings
+let age=22;
+let firstname="Mohd Ziya";
+// console.log("My name is "+firstname+" and my age is "+age);
+let aboutMe=`My name is ${firstname} and my age is ${age}`
+console.log(aboutMe);
+
