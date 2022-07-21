951d4d08ce9524f20f78fd911298c1d49cf8d353
Mohd Ziya <iamziya53@gmail.com>
2022-07-20 20:03:07 -0400

    practice on loops

--- Namste_Js/Harshit_classes/whileloop.js
+++ Namste_Js/Harshit_classes/whileloop.js
@@ -55,9 +55,19 @@
 
 
 // continue keyword
-for(let i=1;i<=10;i++){
-    if(i===4){
-        continue;
-    }
+// for(let i=1;i<=10;i++){
+//     if(i===4){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+
+
+
+//    do-while loops
+let i=1;
+do{
     console.log(i);
-}
+    i++;
+}while(i<=9)
