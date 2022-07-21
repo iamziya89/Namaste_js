5b02cb5683bb538bf193175e03eee412e514d458
Mohd Ziya <iamziya53@gmail.com>
2022-07-21 00:40:11 -0400

    practice on loops

---
+++ Namste_Js/Harshit_classes/arra_01.js
@@ -0,0 +1,15 @@
+//   Introduction to Array
+// let fruits=["Apple","Mango","Grapes","Orange"];
+// console.log(fruits);
+// console.log(fruits[0]);
+// console.log(fruits[1]);
+// console.log(fruits[2]);
+// console.log(fruits[3]);
+
+
+let fruits=["Apple","Mango","Grapes","Orange"];
+// console.log(fruits);
+// fruits[1]="Mohd Ziya";
+// console.log(fruits);
+// console.log(typeof fruits);
+console.log(Array.isArray(fruits));
