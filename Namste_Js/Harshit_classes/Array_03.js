19bd0bcad7e93193638d17e2f6c9bb34e54d0eca
Mohd Ziya <iamziya53@gmail.com>
2022-07-21 20:05:55 -0400

    practice on array

---
+++ Namste_Js/Harshit_classes/Array_03.js
@@ -0,0 +1,33 @@
+// let fruits=["Apple","Mango","Grapes","Orange"];
+// fruits.unshift("Mohd")
+// console.log(fruits);
+// fruits.unshift('Banana')
+// fruits.unshift('Mangoesedd')
+// console.log(fruits);
+
+// fruits.shift();
+// fruits.shift();
+// fruits.shift();
+// console.log(fruits);
+
+// let fruits=["Apple","Mango","Grapes","Orange"];
+// let rem=fruits.shift();
+// console.log(rem);
+
+
+//   Primitive VS reference data types
+//   PRIMITIVE 
+// let n1=6;
+// let n2=n1;
+// console.log(n1);
+// console.log(n2);
+// n1++;
+// console.log("After incrementing the values");
+// console.log(n1);
+// console.log(n2);
+
+
+
+//   Reference Types
+
+
