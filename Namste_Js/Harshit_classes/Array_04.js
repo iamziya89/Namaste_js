f983ac645f8f3af848ac033e7b880a5fd49ff95b
Mohd Ziya <iamziya53@gmail.com>
2022-07-22 12:34:31 -0400

    practice on array

---
+++ Namste_Js/Harshit_classes/Array_04.js
@@ -0,0 +1,20 @@
+//   How to clone array
+// let arr1=["item 1", "item 2"];
+// arr1.push("item 3")
+// let arr2=arr1;
+// console.log(arr1,arr2)
+// console.log(arr1===arr2);
+
+
+// let arr1=["item 1", "item 2"];
+// let arr2=["item 1", "item 2"];
+// arr1.push("item 3");
+// console.log(arr1,arr2);
+// console.log(arr1===arr2)
+
+
+let a1=["Mohd","Ziya"]
+let a2=a1.slice(0);
+console.log(a1===a2);
+a1.push("Arbaz")
+console.log(a1,a2);
