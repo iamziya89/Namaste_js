33ecfba331e598789d330e284ae7d800a1f15119
Mohd Ziya <iamziya53@gmail.com>
2022-07-25 16:15:19 -0400

    Array Concept

--- Namste_Js/Harshit_classes/Array_04.js
+++ Namste_Js/Harshit_classes/Array_04.js
@@ -13,8 +13,23 @@
 // console.log(arr1===arr2)
 
 
-let a1=["Mohd","Ziya"]
-let a2=a1.slice(0);
-console.log(a1===a2);
-a1.push("Arbaz")
-console.log(a1,a2);
+// let a1=["Mohd","Ziya"]
+// let a2=a1.slice(0);
+// console.log(a1===a2);
+// a1.push("Arbaz")
+// console.log(a1,a2);
+
+
+//    SPREAD OPERATOR
+// let arr1=["1","2"];
+// let arr2=[...arr1];
+// arr1.push("3");
+// console.log(arr1,arr2,arr1===arr2)
+
+
+
+let arr1=["1","2"];
+// let arr2=[].concat(arr1);
+// console.log(arr1,arr2);
+let arr2=[...arr1];
+console.log(arr1,arr2);
---
+++ Namste_Js/Harshit_classes/Array_05.js
@@ -0,0 +1,13 @@
+// var arr=[];
+// arr[0]="rahul"
+// arr[1]="ram"
+// arr[2]=56
+// arr[3]="ziya"
+// arr[42]='Anas'
+// console.log(arr);
+// console.log(arr.length);
+// console.log(arr[0])
+
+
+var arr=[,,]
+console.log(arr[0],arr[1],arr[2],arr[3])
