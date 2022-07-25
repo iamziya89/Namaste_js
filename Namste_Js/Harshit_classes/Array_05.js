bc7b32cd69feeed7e6a0cb1ccc0d10e67e7ceb6a
Mohd Ziya <iamziya53@gmail.com>
2022-07-25 17:04:10 -0400

    Array Concept

--- Namste_Js/Harshit_classes/Array_05.js
+++ Namste_Js/Harshit_classes/Array_05.js
@@ -20,5 +20,26 @@
 // console.log(arr)
 // console.log(arr[0],arr[1],arr[2],arr[3],arr[4]);
 
-var arr=new Array(5)
-console.log(arr)
+// var arr=new Array(5)   is not equal to var arr=[5] because hum new ke saath sirf ek value nahi de sakte hai
+// console.log(arr)
+
+// var arr=[10,20,30,40,50]
+// console.log(arr[2],arr[`2`]);
+
+// var arr=[]
+// arr[0]=10;
+// arr[1]=20;
+// arr[2]=30;
+// arr[3]=40;
+// arr[4];
+// console.log(arr[4]);
+
+// var arr=new Array(10,20,30,40);
+// arr[0]=79;
+// console.log(arr);
+
+// var geek=["Rahul","Ram",56,68,"Ramesh"]
+// var arr=geek;
+// geek[0]=10;
+// console.log(geek,arr);
+
