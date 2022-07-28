630144018085afa6980b5d9446f27d99b55a8252
Mohd Ziya <iamziya53@gmail.com>
2022-07-27 20:24:33 -0400

    String concept

--- Namste_Js/Harshit_classes/Array_06.js
+++ Namste_Js/Harshit_classes/Array_06.js
@@ -3,9 +3,18 @@
 // var fullName=fname+" "+lname;
 // console.log(fullName);
 
-console.log("Hellow".concat(" ","Mohd"," ","Ziya"));
+// console.log("Hellow".concat(" ","Mohd"," ","Ziya"));
 
-var str1="Mohammad";
-var str2=" Ziya"
-var str3=str1.concat(str2)
-console.log(str3);
+// var str1="Mohammad";
+// var str2=" Ziya"
+// var str3=str1.concat(str2)
+// console.log(str3);
+
+
+// console.log("Mohd
+// Ziya");
+// console.log(`Mohd
+//                   Ziya`);
+
+let str="Ziya"
+console.log(`Hello ${str}`);
