726a8877f0800f2ea85cbc82c01530307b2d41c0
Mohd Ziya <iamziya53@gmail.com>
2022-08-01 17:02:18 -0400

    String concept

--- Namste_Js/Harshit_classes/Obj1.js
+++ Namste_Js/Harshit_classes/Obj1.js
@@ -1,12 +1,47 @@
-var fees={};
-fees['Mohd']=100;
-fees['Ziya']=200;
-fees['Shameem']=300;
-// console.log(fees);
-// // console.log(fees.Ziya);
-// console.log(fees["Ziya"]);
-// console.log(fees.Mohd);
-// console.log(fees.Shameem);
-// console.log(fees['Shameem']);
-console.log(Object.keys(fees));
+// var fees={};
+// fees['Mohd']=100;
+// fees['Ziya']=200;
+// fees['Shameem']=300;
+// // console.log(fees);
+// // // console.log(fees.Ziya);
+// // console.log(fees["Ziya"]);
+// // console.log(fees.Mohd);
+// // console.log(fees.Shameem);
+// // console.log(fees['Shameem']);
+// console.log(Object.keys(fees));
+
+// const person={
+//     name:"Ziya",
+//     age:25,
+// }
+// console.log(person);
+// console.log(typeof person);
+// console.log(person.name);
+// console.log(person.age);
+
+
+const person={
+    name:"Ziya",
+    age:25,
+    hobbies:["Music","Cricket","Football"]
+}
+// // console.log(person.hobbies[1]);
+// person.gender="Male"
+// console.log(person);
+
+console.log(person["hobbies"]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
